4c253f56bfb321d625b71d45073047bba93b1acf
ivyan26733 <ds26733@gmail.com>
2026-03-02 23:46:27 +0530

    Fixed Websocket Bug

--- useExecutionSocket.ts
+++ useExecutionSocket.ts
@@ -17,6 +17,14 @@ const WS_URL =
     : null) ??
   'http://localhost:8090/ws'
 
+if (typeof window !== 'undefined') {
+  // Basic runtime logger so we can see which WS URL the client is using in prod/dev.
+  // NOTE: For SockJS this MUST be http(s), not ws(s).
+  // If you see a ws:// or wss:// value here, fix NEXT_PUBLIC_WS_URL / NEXT_PUBLIC_API_URL.
+  // eslint-disable-next-line no-console
+  console.info('[useExecutionSocket] WS_URL resolved to', WS_URL)
+}
+
 interface Options {
   executionId: string | null
   onEvent: (event: NodeExecutionEvent) => void
@@ -34,18 +42,36 @@ export function useExecutionSocket({ executionId, onEvent }: Options) {
       webSocketFactory: () => new SockJS(WS_URL),
       onConnect: () => {
         const topic = `/topic/execution/${executionId}`
+        // eslint-disable-next-line no-console
+        console.info('[useExecutionSocket] STOMP connected, subscribing to', topic)
         client.subscribe(topic, (msg) => {
-          const event: NodeExecutionEvent = JSON.parse(msg.body)
-          onEventRef.current(event)
+          try {
+            const event: NodeExecutionEvent = JSON.parse(msg.body)
+            onEventRef.current(event)
+          } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error('[useExecutionSocket] Failed to parse WS event', e, msg.body)
+          }
         })
       },
-      onStompError: () => {},
-      onDisconnect: () => { clientRef.current = null },
+      onStompError: (frame) => {
+        // eslint-disable-next-line no-console
+        console.error('[useExecutionSocket] STOMP error', frame.headers, frame.body)
+      },
+      onDisconnect: () => {
+        // eslint-disable-next-line no-console
+        console.info('[useExecutionSocket] STOMP disconnected')
+        clientRef.current = null
+      },
     })
 
+    // eslint-disable-next-line no-console
+    console.info('[useExecutionSocket] Activating STOMP client...')
     client.activate()
     clientRef.current = client
     return () => {
+      // eslint-disable-next-line no-console
+      console.info('[useExecutionSocket] Deactivating STOMP client...')
       clientRef.current?.deactivate()
       clientRef.current = null
     }
